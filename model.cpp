d0d769650d02afef47a53521a86ab7ec203a4832
谷本康輔 <211104@st.yoshida-g.ac.jp>
2022-11-07 19:00:50 +0900

    環境オブジェクト追加

--- model.cpp
+++ model.cpp
@@ -37,6 +37,8 @@ char*			CModel::m_pModelPass[MODEL_MAX] =
 	{ "data\\MODELS\\Coin\\Coin02.x" },
 	{ "data\\MODELS\\Coin\\Coin03.x" },
 
+	{ "data\\MODELS\\Mountains\\Fuji.x" },
+
 	{ "data\\MODELS\\Stage_Debug\\Stage_Debug_Obstade.x" },
 	{ "data\\MODELS\\Stage_Debug\\Stage_Debug_Obstade_Ball.x" },
 	{ "data\\MODELS\\Stage_Debug\\Stage_Debug_Obstade_Cylinder.x" },
